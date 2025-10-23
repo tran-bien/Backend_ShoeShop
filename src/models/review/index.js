afc1ab292be2c764f7b8f6ec7205a04447c28f49
tran-bien <bientnb2k3@gmail.com>
2025-10-23 16:31:38 +0700

    update

--- src/models/review/index.js
+++ src/models/review/index.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 const ReviewSchema = require("./schema");
-const { applyMiddlewares } = require("./middlewares");
 const softDeletePlugin = require("@plugins/softDelete");
 
 // Áp dụng plugin xóa mềm
 ReviewSchema.plugin(softDeletePlugin);
-
-// Áp dụng middlewares
-applyMiddlewares(ReviewSchema);
 
 // Tạo model
 const Review = mongoose.model("Review", ReviewSchema);
